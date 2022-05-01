e312d5ab4e97a4b5ecc3f1badc84524949e06530
Khalandr <48185617+Khalandr@users.noreply.github.com>
2022-05-01 10:07:59 +0200

    Update FirstClass.java
    comment is added

--- java_p1/src/FirstClass.java
+++ java_p1/src/FirstClass.java
@@ -1,5 +1,7 @@
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.print("Hello World!");
+        System.out.print("Hello World!");
+        //this is comments
     }
 }

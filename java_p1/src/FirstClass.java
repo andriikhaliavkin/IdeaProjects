032217141e5b0d0e68bea5a7a3d9d7d81642d0f4
Khalandr <48185617+Khalandr@users.noreply.github.com>
2022-05-01 10:14:54 +0200

    Update FirstClass.java
    add static int a var and print it

--- java_p1/src/FirstClass.java
+++ java_p1/src/FirstClass.java
@@ -1,7 +1,8 @@
 public class FirstClass {
+    static int a = 4;
     public static void main(String[] args) {
-        System.out.print("Hello World!");
-        System.out.print("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println(a);
         //this is comments
     }
 }

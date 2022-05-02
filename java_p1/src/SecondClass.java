0a8b43254256d1d027a8e8b50d9ef8b8d386c9b3
Khalandr <48185617+Khalandr@users.noreply.github.com>
2022-05-02 08:02:56 +0200

    Update SecondClass.java
    setData method

--- java_p1/src/SecondClass.java
+++ java_p1/src/SecondClass.java
@@ -1,11 +1,9 @@
 public class SecondClass {
-    public void getData() {
-        System.out.println("I'm in second calss");
+    public void setData() {
+        System.out.println("I'm in second class method");
     }
 
     public static void main(String[] args){
-        SecondClass fn = new SecondClass();
-        fn.getData();
 
     }
 }

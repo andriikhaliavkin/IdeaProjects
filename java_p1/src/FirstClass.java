6f0cefa2301cec711ec15ab9beae61c5a49208e9
Khalandr <48185617+Khalandr@users.noreply.github.com>
2022-05-02 08:02:43 +0200

    Update FirstClass.java
    accessing method from another class

--- java_p1/src/FirstClass.java
+++ java_p1/src/FirstClass.java
@@ -1,11 +1,13 @@
 public class FirstClass {
     static int a = 4;
     public void getData() {
-        System.out.println("I'm in method");
+        System.out.println("I'm in method first class");
     }
     public static void main(String[] args) {
         FirstClass fn = new FirstClass();
         fn.getData();
+        SecondClass sn = new SecondClass();
+        sn.setData();
 //        System.out.println(fn.a);
 
 

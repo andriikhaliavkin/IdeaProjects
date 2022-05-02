1598342b85cd5d10702286092d6313b4b925ca97
Khalandr <48185617+Khalandr@users.noreply.github.com>
2022-05-02 07:57:08 +0200

    Create SecondClass.java
    second class

---
+++ java_p1/src/SecondClass.java
@@ -0,0 +1,11 @@
+public class SecondClass {
+    public void getData() {
+        System.out.println("I'm in second calss");
+    }
+
+    public static void main(String[] args){
+        SecondClass fn = new SecondClass();
+        fn.getData();
+
+    }
+}

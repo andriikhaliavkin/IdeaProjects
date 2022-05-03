aa638d1d20777c35583db80ec820452e2b77ca23
Khalandr <48185617+Khalandr@users.noreply.github.com>
2022-05-03 10:42:13 +0200

    Update StringClassDemo.java
    def the string literal

--- java_p1/src/StringClassDemo.java
+++ java_p1/src/StringClassDemo.java
@@ -1,5 +1,7 @@
 public class StringClassDemo {
+    static String a = "Hello"; //defining the String Literal
+    static String b = "Hello"; //refers a to the b object rather than creating one more object separately.
     public static void main(String[] args){
-
+        System.out.println(b);
     }
 }

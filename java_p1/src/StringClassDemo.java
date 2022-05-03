029ca167c84e1d3945e8a3c8564fb62b58aeefe1
Khalandr <48185617+Khalandr@users.noreply.github.com>
2022-05-03 21:31:23 +0200

    Update StringClassDemo.java
    string methods

--- java_p1/src/StringClassDemo.java
+++ java_p1/src/StringClassDemo.java
@@ -1,9 +1,11 @@
 public class StringClassDemo {
 //    static String a = "Hello"; //defining the String Literal
 //    static String b = "Hello"; //refers a to the b object rather than creating one more object separately.
-    static String ab=new String("hello2");
-    static String av=new String("hello2");
+    static String a=new String("hello2");
+    static String b=new String("hello2");
     public static void main(String[] args){
-        System.out.println(av);
+        System.out.println(a);
+        System.out.println(a.charAt(2));
+        System.out.println(a.toUpperCase());
     }
 }

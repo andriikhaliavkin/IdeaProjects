06adc4cabe6bcbea4b9ffceef982b04ab3d0b07d
Khalandr <48185617+Khalandr@users.noreply.github.com>
2022-05-03 21:35:19 +0200

    Update StringClassDemo.java

--- java_p1/src/StringClassDemo.java
+++ java_p1/src/StringClassDemo.java
@@ -2,10 +2,14 @@ public class StringClassDemo {
 //    static String a = "Hello"; //defining the String Literal
 //    static String b = "Hello"; //refers a to the b object rather than creating one more object separately.
     static String a=new String("hello2");
-    static String b=new String("hello2");
+    static String b=new String("some rendom text");
     public static void main(String[] args){
         System.out.println(a);
         System.out.println(a.charAt(2));
         System.out.println(a.toUpperCase());
+        System.out.println(b.indexOf("o"));
+        System.out.println(b.substring(3, 6));
+        System.out.println(b.substring(3));
+
     }
 }

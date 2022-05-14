58e3665abe0567b4ec135372fb447698d4bb6477
Khalandr <48185617+Khalandr@users.noreply.github.com>
2022-05-14 07:43:09 +0200

    init hello world

--- java_p1/src/StringClassDemo.java
+++ java_p1/src/StringClassDemo.java
@@ -2,7 +2,7 @@ public class StringClassDemo {
 //    static String a = "Hello"; //defining the String Literal
 //    static String b = "Hello"; //refers a to the b object rather than creating one more object separately.
     static String a=new String("hello2");
-    static String b=new String("some rendom text");
+    static String b=new String("some random text");
     public static void main(String[] args){
         System.out.println(a);
         System.out.println(a.charAt(2));
